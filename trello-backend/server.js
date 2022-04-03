fdebb571e8355f836cfc2eb0d04054e4f313d0e0
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-03 18:15:15 -0400

    Set-up middleware and listening PORT

--- trello-backend/server.js
+++ trello-backend/server.js
@@ -1,5 +1,22 @@
+/*
+    Imports dependencies
+*/
 require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+
+/*
+    Middleware
+*/
+app.use(cors());
+app.use(express.json());
+
+
+/*
+    Listening Port
+*/
+app.listen(PORT, () => {
+    `I am running on PORT: ${PORT}`;
+})
