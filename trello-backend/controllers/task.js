98af57c95e0e573da3a1fe7d715e8350703cf013
ptnguyen617 <ptnguyen617@yahoo.com>
2022-04-03 19:08:02 -0400

    Created show route

--- trello-backend/controllers/task.js
+++ trello-backend/controllers/task.js
@@ -5,4 +5,22 @@ const express = require('express');
 const router = express.Router();
 const Task = require('../models/Task');
 
+/*
+    Create, Read, Update, Delete
+*/
+
+/*
+    SHOW PAGE
+*/
+router.get('/:id', (req, res) => {
+    Task.findById(req.params.id, (err, foundTask) => {
+        if(!err) {
+            res.status(200).json(foundTask);
+        }else {
+            res.status(400).send(err);
+        }
+    });
+});
+
+
 module.exports = router;
